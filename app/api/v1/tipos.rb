f7355939b95f9079ea7edaf31061df3ce03b7e24
Marlon Silva Carvalho <marlon.carvalho@gmail.com>
2015-06-26 08:56:40 -0300

    Correções na API de Tipos.

--- app/api/v1/tipos.rb
+++ app/api/v1/tipos.rb
@@ -19,11 +19,22 @@ module API
           tipo.nome = params[:nome]
           tipo.save
 
+          status 201
+          header 'Location', "/tipos/#{tipo.id}"
           present tipo, :with => Entities::Tipo
         end
 
         put ':id' do
-          tipo = Models::Tipo.get! params[:id]
+          tipo = Models::Tipo.get params[:id]
+
+          if tipo.nil?
+            status 201
+            header 'Location', "/tipos/#{tipo.id}"
+            tipo = Models::Tipo.new
+          else
+            status 200
+          end
+
           tipo.nome = params[:nome]
           tipo.save
 
